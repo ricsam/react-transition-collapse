21d310cfbbfcb68d1733a074051df4a50f78649e
Richard Samuelsson <richard.samuelsson@redeye.se>
2019-03-18 17:36:13 +0100

    feat(wip):

--- example/src/index.js
+++ example/src/index.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import ReactTransitionCollapse from 'react-transition-collapse'
 
 const Index = () => {
-  return <ReactTransitionCollapse />
+  const [expanded, setExpanded] = useState(false)
+  return (
+    <>
+      <button onClick={() => setExpanded(expanded => !expanded)}>Expand</button>
+      <div style={{ width: '300px' }}>
+        <ReactTransitionCollapse expanded={expanded}>
+          Lorem nostrud velit ullamco dolore exercitation consectetur occaecat enim laboris cillum
+          incididunt ullamco ex. Adipisicing eu nulla anim laborum. Exercitation consequat anim
+          culpa aute fugiat dolor in aliqua Lorem labore mollit anim id dolore. Sunt ut sunt duis
+          commodo irure dolore adipisicing occaecat non nisi sit. Labore consequat amet anim nulla
+          ipsum excepteur do duis labore.
+        </ReactTransitionCollapse>
+      </div>
+    </>
+  )
 }
 
 ReactDOM.render(<Index />, document.getElementById('root'))
--- src/react-transition-collapse.tsx
+++ src/react-transition-collapse.tsx
@@ -4,9 +4,65 @@
 
 import * as React from 'react'
 
-class ReactTransitionCollapse extends React.PureComponent {
+type styleType = {
+  transition: string
+  overflow: string
+  height: string
+}
+
+const wrapperStyle: styleType = {
+  transition: 'height 0.5s ease',
+  overflow: 'hidden',
+  height: 'auto'
+}
+
+type DomEl = null | HTMLDivElement
+
+class ReactTransitionCollapse extends React.PureComponent<{
+  expanded: boolean
+  children: Element
+}> {
+  el: DomEl = null
+
+  state = {
+    initialRender: true
+  }
+
+  componentDidMount() {
+    this.setState({
+      initialRender: false
+    })
+  }
+
+  setHeight = (el: DomEl) => {
+    this.el = el
+
+    if (!el) {
+      return
+    }
+
+    this.setState({
+      height: el.offsetHeight
+    })
+  }
+
   render() {
-    return <div>React test, feat</div>
+    const { expanded, children } = this.props
+    const { initialRender } = this.state
+
+    const style = {
+      ...wrapperStyle
+    }
+
+    if (!initialRender) {
+      style.height = ''
+    }
+
+    return (
+      <div style={style} ref={this.setHeight}>
+        {children}
+      </div>
+    )
   }
 }
 
